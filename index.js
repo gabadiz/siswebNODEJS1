b294e6d5f3dade9fa5e2385b8fc0c5c2b3b92fe1
Gabriel Díaz Arevalillo <114990592+gabadiz@users.noreply.github.com>
2025-10-25 20:11:41 +0200

    casi terminao

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
-const { set } = require('express/lib/application');
+
 const http = require('http');
 const os = require('os');
-
+const time=3000;
 const server = http.createServer((req, res) => {
     
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello, World!</h1>');
+    res.end(`
+        <h1>Hello, World!</h1> 
+        <label for="numero">Introduce un numero:</label> 
+        <input type="number" id="numero" name="numero"></input>
+        <button type="submit">Enviar</button>
+    `);
+
 
     
 });
 server.listen(3000, () => {
-    function ejercicio() {
-        console.log('New connection')
     
-        //console.log('Uso de CPU: '+JSON.stringify(os.cpus(), null, 1))
+
+    console.log('New connection')
+    function ejercicio() {
+        
+        //console.log('Uso de CPU: '+JSON.stringify(os.cpus(), null, 1))const cpus = os.cpus();
+        const cpus = os.cpus();
+        let idle = 0;
+        let total = 0;
+
+        for (let i = 0; i < cpus.length; i++) {
+            const times = cpus[i].times;
+            for (let t in times) {
+                total += times[t];
+            }
+            idle += times.idle;
+        }
+
+        const usage = (1 - idle / total) * 100;
+        console.log('Uso de CPU: ' + usage + '%');
+
+            
 
         const totalMem = os.totalmem();
-        const memory = process.memoryUsage();
-        const memoryUsage = (memory.rss / totalMem) * 100;
-        console.log('Porcentaje de memoria usada: ' + memoryUsage + '%');
+        const freeMem = os.freemem();
+        const usedMem = totalMem - freeMem;
+        const usedPercent = (usedMem / totalMem) * 100;
+        console.log('Porcentaje de memoria usada: ' + usedPercent + '%');
 
-        console.log('Tiempo de procesamiento: ' + process.uptime())
+        console.log('Tiempo de procesamiento: ' + process.uptime()+' segundos');
 
-        console.log('Tiempo de actividad del sistema: ' + os.uptime())
+        console.log('Tiempo de actividad del sistema: ' + os.uptime()+' segundos');
+        
+
     }
     ejercicio();
-    setInterval(ejercicio, 3000);
+    setInterval(ejercicio, time);
 });
