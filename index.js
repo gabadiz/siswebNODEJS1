e58c935a748f240e530194fab94bc58e64d07d77
Gabriel Díaz Arevalillo <114990592+gabadiz@users.noreply.github.com>
2025-10-26 11:55:08 +0100

    last changes index js

--- index.js
+++ index.js
@@ -1,57 +1,101 @@
-
 const http = require('http');
 const os = require('os');
-const time=3000;
+
+let time = 3000; 
+let interval;
+
 const server = http.createServer((req, res) => {
-    
+
+  if (req.url === '/') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end(`
-        <h1>Hello, World!</h1> 
-        <label for="numero">Introduce un numero:</label> 
-        <input type="number" id="numero" name="numero"></input>
-        <button type="submit">Enviar</button>
+
+
+      <label for="numero">Introduce un numero:</label> 
+    
+      <input type="number" id="numero" name="numero"></input>
+      <button type="submit" id="boton">Enviar</button>
+
+      <script>
+        const boton = document.getElementById('boton');
+        boton.addEventListener('click', () => {
+          const numero = document.getElementById('numero').value;
+          if(numero < 1) {
+            alert('El numero debe ser mayor o igual a 1 segundo');
+            return;
+          }
+          // Enviar el número al servidor
+          fetch('/actualizar?time=' + numero)
+            .then(res => res.text())
+            .then(msg => alert(msg));
+        });
+      </script>
     `);
+  }
 
 
-    
-});
-server.listen(3000, () => {
-    
+  else if (req.url.startsWith('/actualizar')) {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const nuevoTiempo = parseInt(url.searchParams.get('time')) * 1000;
 
-    console.log('New connection')
-    function ejercicio() {
-        
-        //console.log('Uso de CPU: '+JSON.stringify(os.cpus(), null, 1))const cpus = os.cpus();
-        const cpus = os.cpus();
-        let idle = 0;
-        let total = 0;
+    if (isNaN(nuevoTiempo) || nuevoTiempo < 1000) {
 
-        for (let i = 0; i < cpus.length; i++) {
-            const times = cpus[i].times;
-            for (let t in times) {
-                total += times[t];
-            }
-            idle += times.idle;
-        }
+      res.statusCode = 400;
 
-        const usage = (1 - idle / total) * 100;
-        console.log('Uso de CPU: ' + usage + '%');
-
-            
-
-        const totalMem = os.totalmem();
-        const freeMem = os.freemem();
-        const usedMem = totalMem - freeMem;
-        const usedPercent = (usedMem / totalMem) * 100;
-        console.log('Porcentaje de memoria usada: ' + usedPercent + '%');
-
-        console.log('Tiempo de procesamiento: ' + process.uptime()+' segundos');
-
-        console.log('Tiempo de actividad del sistema: ' + os.uptime()+' segundos');
-        
-
+      return res.end('El tiempo debe ser mayor o igual a 1 segundo');
     }
-    ejercicio();
-    setInterval(ejercicio, time);
+
+    time = nuevoTiempo;
+
+    clearInterval(interval);
+
+    startInterval();
+
+    res.statusCode = 200;
+
+    res.end('Tiempo actualizado a ' + (time / 1000) + ' segundos');
+  }
+});
+
+function ejercicio() {
+  const cpus = os.cpus();
+  let idle = 0;
+
+  let total = 0;
+
+  for (let i = 0; i < cpus.length; i++) {
+
+    const times = cpus[i].times;
+
+    for (let t in times){ total = total+times[t]};
+
+    idle = idle + times.idle;
+  }
+
+  const usage = (1 - idle / total) * 100;
+  console.log('Uso de CPU: ' + usage + '%');
+
+  const totalMem = os.totalmem();
+  const freeMem = os.freemem();
+  const usedMem = totalMem - freeMem;
+  const usedPercent = (usedMem / totalMem) * 100;
+
+  console.log('Porcentaje de memoria usada: ' + usedPercent + '%');
+
+  console.log('Tiempo de procesamiento: ' + process.uptime() + ' segundos');  
+
+  console.log('Tiempo de actividad del sistema: ' + os.uptime() + ' segundos');
+  console.log('------------------------------------');
+}
+
+function startInterval() {
+  ejercicio();
+  
+  interval = setInterval(ejercicio, time);
+}
+
+server.listen(3000, () => {
+  console.log('New connection');
+  startInterval();
 });
